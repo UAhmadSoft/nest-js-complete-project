3421b72028a099fb81b96214b3373034b8b78f8f
UAhmadSoft <umadahmad1928@gmail.com>
2022-09-01 11:14:13 +0500

    Implemente websockets by gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,18 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './user/user.schema';
 import { TodoModule } from './todos/todos.module';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
 import { LoggerMiddleware } from './middlewares/logger';
-import { LoggerModule } from 'nestjs-pino';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { NotificationsGateway } from './notifications.gateway';
 @Module({
   imports: [
     ThrottlerModule.forRoot({
       ttl: 60 * 60 * 1000, // 1 minute
       limit: 100,
     }),
-    LoggerModule.forRoot(),
+    // LoggerModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['config.env'],
@@ -35,6 +32,7 @@ import { APP_GUARD } from '@nestjs/core';
   ],
   controllers: [],
   providers: [
+    NotificationsGateway,
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
---
+++ src/notifications.gateway.ts
@@ -0,0 +1,40 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+
+@WebSocketGateway({
+  namespace: 'notifications',
+})
+export class NotificationsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server;
+  users: number = 0;
+
+  async handleConnection() {
+    // A client has connected
+    this.users++;
+
+    // Notify connected clients of current users
+    this.server.emit('users', this.users);
+    console.log(this.users);
+  }
+
+  async handleDisconnect() {
+    // A client has disconnected
+    this.users--;
+
+    // Notify connected clients of current users
+    this.server.emit('users', this.users);
+  }
+
+  @SubscribeMessage('chat')
+  async onChat(client, message) {
+    client.broadcast.emit('chat', message);
+  }
+}
